3b17a362cf271389b953ee6638bc0eeef5b6d905
Bhanu pratap <piersquare0909@gmail.com>
2025-12-17 23:39:19 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (80.31%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,4 +1,6 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
+        # this solution uses inbuild function in python
+        #other solution is result = x ** 0.5
         result = math.sqrt(x)
         return (int(result))
