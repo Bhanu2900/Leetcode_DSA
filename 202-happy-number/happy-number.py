6471d3e60eb5e8ffb1c58ded2e44cab5147b30cb
Bhanu pratap <piersquare0909@gmail.com>
2025-12-18 22:32:19 +0530

    Time: 3 ms (41.45%) | Memory: 17.2 MB (99.99%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -2,9 +2,6 @@ class Solution:
     def isHappy(self, n: int) -> bool:
         def sum_sq(num: int) -> int:
             return sum(int(digit)** 2 for digit in str(num))
-        cycle = set()
-
-        while  n != 1 and n not in cycle:
-            cycle.add(n)
+        while  n != 1 and n != 4:
             n = sum_sq(n)
         return n == 1
