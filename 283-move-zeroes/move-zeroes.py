2f70212a57a44aa2aa5a9d5758ffa5433f73771e
Bhanu pratap <piersquare0909@gmail.com>
2025-12-26 00:41:05 +0530

    Time: 4 ms (59.64%) | Memory: 18.6 MB (94.88%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -1,6 +1,6 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        # here we take all the non- zeros values by comparing, save them first
+        # here we take all the non- zeros values by comparing, save them first and then add all the remaing zeros to the end with the help of the loop
         """
         Do not return anything, modify nums in-place instead.
         """
